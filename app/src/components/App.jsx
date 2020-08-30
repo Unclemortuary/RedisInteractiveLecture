0e7f515d3bac5eeded90adf8d49aa4d52cfc44a2
Artem Gusev <Artem_Gusev@epam.com>
2020-08-30 16:25:10 +0300

    empty ui project infrastructure

---
+++ app/src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+    console.log("App init");
+
+    return <span>Tetflix</span>
+};
+
+export default App;
---
+++ app/webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: "./src/main/index.js",
+    output: {
+        path: `${__dirname}/dist`,
+        filename: "[name].js"
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Tetflix",
+            filename: "../tetflix.html",
+            template: "./src/main/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
